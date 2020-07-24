43dd9b3d4c5eca07f33f596fb3bfb4fa7e3bfe2e
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-24 15:12:13 +0100

    type question

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,15 @@
 
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answer: string[];
+    question: String;
+    type: String
+}
+
+
+
 export enum Difficulty {
     EASY = "easy",
     MEDIUM = "medium",
