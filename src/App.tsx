1edc11d6d11132276015e80602efde99a60af78f
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-24 16:15:45 +0100

    staging the state of the app

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,22 @@ function App() {
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.MEDIUM))
+  // console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.MEDIUM))
 
   const startQuiz = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.MEDIUM
+    );
+
+    setQuestions(newQuestions)
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false)
 
   }
 
@@ -39,19 +52,23 @@ function App() {
   return (
     <div className="App">
       <h1>Quiz</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+
       <button className="start" onClick={startQuiz}>
         Start
       </button>
-      <p className = "score">Score:</p>
-      <p>Loading Questions ...</p>
-      <QuestionCard 
-      // questionNr={number + 1}
-      // totalQuestions = {TOTAL_QUESTIONS}
-      // question={questions[number].question}
-      // answers={questions[number].answers}
-      // userAnswer={userAnswers ? userAnswers[number] : undefined}
-      // callBack={checkAnswer}
-      />
+      ) : null
+      }
+      {! gameOver ? <p className = "score">Score:</p> : null }
+      {loading ? <p>Loading Questions ...</p> : null}
+      {/* <QuestionCard 
+      questionNr={number + 1}
+      totalQuestions = {TOTAL_QUESTIONS}
+      questions={questions[number].question}
+      answers={questions[number].answers}
+      userAnswer={userAnswers ? userAnswers[number] : undefined}
+      callBack={checkAnswer}
+      /> */}
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
