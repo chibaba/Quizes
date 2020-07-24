6fc34fa073d1883f326acbef69fef67b3f8b99f8
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-24 12:07:33 +0100

    fetching 3rd party api to display questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import { fetchQuizQuestions } from './API';
 import QuestionCard from './components/QuestionCard';
+import  {Difficulty} from './API';
+
+
+const TOTAL_QUESTIONS = 20;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.MEDIUM))
 
   const startQuiz = async () => {
 
@@ -23,7 +37,14 @@ function App() {
       </button>
       <p className = "score">Score:</p>
       <p>Loading Questions ...</p>
-      <QuestionCard />
+      <QuestionCard 
+      // questionNr={number + 1}
+      // totalQuestions = {TOTAL_QUESTIONS}
+      // question={questions[number].question}
+      // answers={questions[number].answers}
+      // userAnswer={userAnswers ? userAnswers[number] : undefined}
+      // callBack={checkAnswer}
+      />
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
