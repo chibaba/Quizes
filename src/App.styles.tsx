1e03f992db505826654a285e59904c11feb2d2f4
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-25 12:38:53 +0100

    styling components

--- src/App.styles.tsx
+++ src/App.styles.tsx
@@ -43,16 +43,30 @@ export const  Wrapper = styled.div`
      background-size: 100%;
      background-clip: text;
 
-     --webkit-background-clip: text;
-     --webkit-text-fill-color: transparent;
-     --moz-background-clip: text;
-     --moz-background-clip: text;
-     --moz-text-fill-color: transparent;
+     -webkit-background-clip: text;
+     -webkit-text-fill-color: transparent;
+     -moz-background-clip: text;
+     -moz-background-clip: text;
+     -moz-text-fill-color: transparent;
      filter: drop-shadow(2px 2px #0085a3);
      font-size: 70px;
+     font-weight: 700px;
      text-align: center;
      margin: 20px;
+ }
 
+ .start, .next {
+     cursor: pointer;
+     background: linear-gradient(180deg, #fff, #ffcc91);
+     border: 2px solid #d38558;
+     box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+     border-radius: 10px;
+     height: 40px
+     margin: 20px 0;
+     padding: 0 40px;
+ }
+ . start {
+     max-width: 200px;
  }
 
 `
